17f87704c8ff3c8e9e9f5fbc9d761231817cbc22
Ho-Ling Poon <holingpoon@gmail.com>
2013-04-14 20:04:50 -0600

    OOP PHP: Basic getters, setters, and print

--- book.php
+++ book.php
@@ -1,20 +1,51 @@
 <?php
     class Book {
-        public $title = '';
+        private $title = '';
 	private $lastname = '';
 	private $firstname = '';
 	private $isbn = '';
 
-	public function __get($property) {
-		if ($property === 'title')
-			return $title;
+	public function __construct($title, $lastname, $firstname, $isbn) {
+		$this->title = $title;
+		$this->lastname = $lastname;
+		$this->firstname = $firstname;
+		$this->isbn = $isbn;
 	}
-	public function __set($property,$value) {
-		if ($property === 'title')
-			$this->title = $value;
+
+	public function setTitle($newTitle) {
+		$this->title = $newTitle;
+	}
+	public function getTitle() {
+		return $this->title;
+	}
+
+	public function setLastName($newLastName) {
+		$this->lastname = $newLastName;
+	}
+	public function getLastName() {
+		return $this->lastname;
+	}
+
+	public function setFirstName($newFirstName) {
+		$this->firstname = $newFirstName;
+	}
+	public function getFirstName() {
+		return $this->firstname;
+	}
+	
+	public function setISBN($newISBN) {
+		$this->isbn = $newISBN;
+	}
+	public function getISBN() {
+		return $this->isbn;
+	}
+	public function printBook() {
+		echo $this->title . "\n" . $this->firstname . " " . $this->lastname . "\n";
+		echo $this->isbn . "\n";
 	}
     }
-    $obj = new Book();
-    $obj->title= "Dandelion Wine";
-    echo $obj->title . "\n";
+    $obj = new Book("Dandelion Wine", "Bradbury", "Ray", "0553277537");
+    //$obj->setTitle("Dandelion Wine");
+    $obj->printBook();
+
 ?>
